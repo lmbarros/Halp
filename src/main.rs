98b899a83cf8844ee6ea56f066a6cf76bc37debc
Leandro Motta Barros <lmb@stackedboxes.org>
2021-11-28 12:40:42 -0300

    Add basic command-line parsing
    Cool, but already almost 15 dependencies added to the project.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::{App, Arg};
+
 fn main() {
+    let matches = App::new("Halp")
+        .version("0.2")
+        .author("Leandro Motta Barros <lmb@stackedboxes.org>")
+        .about("An Ad Hoc Literate Programming Tool")
+        .arg(
+            Arg::with_name("output-dir")
+                .short("o")
+                .long("output-dir")
+                .alias("targetDir") // for backward compatibility
+                .value_name("DIR")
+                .default_value("generated_sources")
+                .help("Sets the directory where the generated files will be written to")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("INPUT")
+                .help("The Halp source files to process")
+                .required(true)
+                .multiple(true)
+                .index(1),
+        )
+        .get_matches();
+
     println!("Hello from Halp!");
+
+    for input in matches.values_of("INPUT").unwrap() {
+        println!("Input file: {}", input)
+    }
 }
